9d0a211f608e01392918c03ac5750f3c321d498e
atbreuer11@gmail.com <atbreuer11@gmail.com>
2020-02-26 17:41:29 -0500

    Updated information and tweaks

--- js/app.js
+++ js/app.js
@@ -5,18 +5,18 @@ function resetSlider(element) {
   element.MaterialSlider.change(element.id);
 }
 
-$( document ).ready(function() {
-  var dialog = document.querySelector('dialog');
-
-  if (! dialog.showModal) {
-    dialogPolyfill.registerDialog(dialog);
-  }
-
-  setTimeout(function() {
-    dialog.showModal();
-   }, 1000);
-
-  dialog.querySelector('.close').addEventListener('click', function() {
-    dialog.close();
-  });
-});
+// $( document ).ready(function() {
+//   var dialog = document.querySelector('dialog');
+//
+//   if (! dialog.showModal) {
+//     dialogPolyfill.registerDialog(dialog);
+//   }
+//
+//   setTimeout(function() {
+//     dialog.showModal();
+//    }, 1000);
+//
+//   dialog.querySelector('.close').addEventListener('click', function() {
+//     dialog.close();
+//   });
+// });
